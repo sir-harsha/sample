8732d22af19bced897be60a13ad9cee890dae664
sri-harsha <sriharsha.b@datagrokr.com>
2024-10-22 12:38:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 class Hello:
     @staticmethod
     def hello_world():
-        print("Hello world")
+        return "Hello world"
+
+if __name__ == '__main__':
+    greeting = Hello.hello_world()
+    print(greeting)
 
